e2af0572af2c164c65a12cdb414817464e924b80
Testing team <chakraborty.indra92@gmail.com>
2023-07-29 20:00:54 +0530

    This is amir first commit

--- src/test/java/testpackage/test1.java
+++ src/test/java/testpackage/test1.java
@@ -6,6 +6,8 @@ public class test1 {
 	
 		System.out.println("Thus is test 1");
 		System.out.println("Thus is test 2");
+		System.out.println("Thus is test 3");
+		System.out.println("Thus is test 4");
 	}
 	
 	public void test_2() {
